906ebdb994ba921efd8f6aa11b4f750cfbd615bc
Manos Zeakis <ezeakis@gmail.com>
2022-02-12 19:11:19 +0200

    Update Common.py

--- Team G/Common.py
+++ Team G/Common.py
@@ -3,15 +3,15 @@ from time import sleep
 sense = SenseHat()
 sense.set_rotation(270)
 
-sense.show_message("my name should be William Thompson ")
+sense.show_message("My name should be William Thompson ", scroll_speed=0.05)
 
-sense.show_message("Do you like space?")
+sense.show_message("Do you like space?", scroll_speed=0.05)
 
-sense.show_message("The humidity is ")
+sense.show_message("The humidity is ", scroll_speed=0.05)
 a = sense.get_humidity()
 a = round(a)
-sense.show_message(str(a))
-sense.show_message("%")
+sense.show_message(str(a), scroll_speed=0.05)
+sense.show_message("%", scroll_speed=0.05)
 
 R = (0, 0,255)
 G = (255, 0, 0)
@@ -40,7 +40,7 @@ R, G, G, G, G, G, G, R,
 Y, R, G, G, G, G, R, Y,
 R, G, G, G, G, G, G, R,
 R, G, G, R, R, G, G, R,
-R, R, R, G, G, R, R, R,
+R, R, R, Y, Y, R, R, R,
 ]
 
 sense.set_pixels(Star_Matrix)
